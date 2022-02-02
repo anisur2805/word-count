97534f2bb5c2edaeae7282d62e4dca58a8d3171c
anisur2805 <anisur2805@gmail.com>
2022-02-02 09:30:25 +0600

    category metabox updated

--- class-metabox.php
+++ class-metabox.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * text-domain: omb-metabox
  */
@@ -7,119 +8,116 @@
  * Undocumented class
  */
 class OMBMetaBox {
- public function __construct() {
-  add_action( 'admin_menu', array( $this, 'omb_metabox' ) );
-  add_action( 'save_post', array( $this, 'omb_save_metabox' ) );
-  add_action( 'save_post', array( $this, 'omb_save_image_metabox' ) );
-  add_action( 'save_post', array( $this, 'omb_save_gallery_metabox' ) );
-  add_action( 'admin_enqueue_scripts', array( $this, 'admin_assets' ) );
- }
-
- public function admin_assets() {
-  wp_enqueue_style( 'metabox-style', WC_DIR_URL_ADMIN . '/css/metabox.css', time() );
-  wp_enqueue_style( 'jquery-ui', '//code.jquery.com/ui/1.13.1/themes/base/jquery-ui.css' );
-  wp_enqueue_script( 'omb-metabox-js', WC_DIR_URL_ADMIN . '/js/metabox.js', array( 'jquery', 'jquery-ui-datepicker' ), time(), true );
- }
-
- private function is_secured( $nonce_field, $action, $post_id ) {
-  $nonce = isset( $_POST[$nonce_field] ) ? $_POST[$nonce_field] : '';
-
-  if ( $nonce == '' ) {
-   return false;
+  public function __construct() {
+    add_action('admin_menu', array($this, 'omb_metabox'));
+    add_action('save_post', array($this, 'omb_save_metabox'));
+    add_action('save_post', array($this, 'omb_save_image_metabox'));
+    add_action('save_post', array($this, 'omb_save_gallery_metabox'));
+    add_action('admin_enqueue_scripts', array($this, 'admin_assets'));
   }
 
-  if ( !wp_verify_nonce( $nonce, $action ) ) {
-   return false;
+  public function admin_assets() {
+    wp_enqueue_style('metabox-style', WC_DIR_URL_ADMIN . '/css/metabox.css', time());
+    wp_enqueue_style('jquery-ui', '//code.jquery.com/ui/1.13.1/themes/base/jquery-ui.css');
+    wp_enqueue_script('omb-metabox-js', WC_DIR_URL_ADMIN . '/js/metabox.js', array('jquery', 'jquery-ui-datepicker'), time(), true);
   }
 
-  if ( !current_user_can( 'edit_post', $post_id ) ) {
-   return false;
+  private function is_secured($nonce_field, $action, $post_id) {
+    $nonce = isset($_POST[$nonce_field]) ? $_POST[$nonce_field] : '';
+
+    if ($nonce == '') {
+      return false;
+    }
+
+    if (!wp_verify_nonce($nonce, $action)) {
+      return false;
+    }
+
+    if (!current_user_can('edit_post', $post_id)) {
+      return false;
+    }
+
+    if (wp_is_post_autosave($post_id)) {
+      return false;
+    }
+
+    if (wp_is_post_revision($post_id)) {
+      return false;
+    }
+
+    return true;
   }
 
-  if ( wp_is_post_autosave( $post_id ) ) {
-   return false;
+  public function omb_save_image_metabox($post_id) {
+    if (!$this->is_secured('omb_image_nonce_field', 'omb_image_action', $post_id)) {
+      return $post_id;
+    }
+
+    $image_id  = isset($_POST['obm_image_id']) ? $_POST['obm_image_id'] : '';
+    $image_url = isset($_POST['obm_image_url']) ? $_POST['obm_image_url'] : '';
+
+    update_post_meta($post_id, 'obm_image_id', $image_id);
+    update_post_meta($post_id, 'obm_image_url', $image_url);
   }
 
-  if ( wp_is_post_revision( $post_id ) ) {
-   return false;
+  public function omb_save_gallery_metabox($post_id) {
+    if (!$this->is_secured('omb_gallery_nonce_field', 'omb_gallery_action', $post_id)) {
+      return $post_id;
+    }
+
+    $images_id  = isset($_POST['obm_images_id']) ? $_POST['obm_images_id'] : '';
+    $images_url = isset($_POST['obm_images_url']) ? $_POST['obm_images_url'] : '';
+
+    update_post_meta($post_id, 'obm_images_id', $images_id);
+    update_post_meta($post_id, 'obm_images_url', $images_url);
   }
 
-  return true;
+  public function omb_save_metabox($post_id) {
 
- }
+    if (!$this->is_secured('omb_location_field', 'omb_location', $post_id)) {
+      return $post_id;
+    }
 
- public function omb_save_image_metabox( $post_id ) {
-  if ( !$this->is_secured( 'omb_image_nonce_field', 'omb_image_action', $post_id ) ) {
-   return $post_id;
+    $location    = isset($_POST['obm_location']) ? $_POST['obm_location'] : '';
+    $country     = isset($_POST['obm_country']) ? $_POST['obm_country'] : '';
+    $is_favorite = isset($_POST['obm_is_favorite']) ? $_POST['obm_is_favorite'] : '';
+    $colors      = isset($_POST['omb_clr']) ? $_POST['omb_clr'] : array();
+    $color       = isset($_POST['omb_color']) ? $_POST['omb_color'] : '';
+
+    $omb_dp        = isset($_POST['omb_dp']) ? $_POST['omb_dp'] : '';
+    $omb_fav_color = isset($_POST['omb_fav_color']) ? $_POST['omb_fav_color'] : '';
+
+    if ('' == $location || '' == $country) {
+      return $post_id;
+    }
+
+    $location = sanitize_text_field($location);
+    $country  = sanitize_text_field($country);
+
+    update_post_meta($post_id, 'obm_location', $location);
+    update_post_meta($post_id, 'obm_country', $country);
+    update_post_meta($post_id, 'obm_is_favorite', $is_favorite);
+    update_post_meta($post_id, 'omb_clr', $colors);
+    update_post_meta($post_id, 'omb_color', $color);
+
+    update_post_meta($post_id, 'omb_dp', $omb_dp);
+    update_post_meta($post_id, 'omb_fav_color', $omb_fav_color);
   }
 
-  $image_id  = isset( $_POST['obm_image_id'] ) ? $_POST['obm_image_id'] : '';
-  $image_url = isset( $_POST['obm_image_url'] ) ? $_POST['obm_image_url'] : '';
-
-  update_post_meta( $post_id, 'obm_image_id', $image_id );
-  update_post_meta( $post_id, 'obm_image_url', $image_url );
-
- }
-
- public function omb_save_gallery_metabox( $post_id ) {
-  if ( !$this->is_secured( 'omb_gallery_nonce_field', 'omb_gallery_action', $post_id ) ) {
-   return $post_id;
+  public function omb_metabox() {
+    add_meta_box('omb_metabox', __('Metabox', 'omb-metabox'), array($this, 'omb_render_metabox'), 'post', 'advanced');
+    add_meta_box('omb_image_info', __('Image Info', 'omb-metabox'), array($this, 'omb_render_image'), 'post', 'advanced');
+    add_meta_box('omb_gallery_info', __('Gallery Info', 'omb-metabox'), array($this, 'omb_render_images'), 'post', 'advanced');
   }
 
-  $images_id  = isset( $_POST['obm_images_id'] ) ? $_POST['obm_images_id'] : '';
-  $images_url = isset( $_POST['obm_images_url'] ) ? $_POST['obm_images_url'] : '';
+  public function omb_render_image($post) {
+    $image_id  = get_post_meta($post->ID, 'obm_image_id', true);
+    $image_url = get_post_meta($post->ID, 'obm_image_url', true);
 
-  update_post_meta( $post_id, 'obm_images_id', $images_id );
-  update_post_meta( $post_id, 'obm_images_url', $images_url );
+    $label = __('Upload Image', 'omb-metabox');
+    wp_nonce_field('omb_image_action', 'omb_image_nonce_field');
 
- }
-
- public function omb_save_metabox( $post_id ) {
-
-  if ( !$this->is_secured( 'omb_location_field', 'omb_location', $post_id ) ) {
-   return $post_id;
-  }
-
-  $location    = isset( $_POST['obm_location'] ) ? $_POST['obm_location'] : '';
-  $country     = isset( $_POST['obm_country'] ) ? $_POST['obm_country'] : '';
-  $is_favorite = isset( $_POST['obm_is_favorite'] ) ? $_POST['obm_is_favorite'] : '';
-  $colors      = isset( $_POST['omb_clr'] ) ? $_POST['omb_clr'] : array();
-  $color       = isset( $_POST['omb_color'] ) ? $_POST['omb_color'] : '';
-
-  $omb_dp        = isset( $_POST['omb_dp'] ) ? $_POST['omb_dp'] : '';
-  $omb_fav_color = isset( $_POST['omb_fav_color'] ) ? $_POST['omb_fav_color'] : '';
-
-  if ( '' == $location || '' == $country ) {
-   return $post_id;
-  }
-
-  $location = sanitize_text_field( $location );
-  $country  = sanitize_text_field( $country );
-
-  update_post_meta( $post_id, 'obm_location', $location );
-  update_post_meta( $post_id, 'obm_country', $country );
-  update_post_meta( $post_id, 'obm_is_favorite', $is_favorite );
-  update_post_meta( $post_id, 'omb_clr', $colors );
-  update_post_meta( $post_id, 'omb_color', $color );
-
-  update_post_meta( $post_id, 'omb_dp', $omb_dp );
-  update_post_meta( $post_id, 'omb_fav_color', $omb_fav_color );
- }
-
- public function omb_metabox() {
-  add_meta_box( 'omb_metabox', __( 'Metabox', 'omb-metabox' ), array( $this, 'omb_render_metabox' ), 'post', 'advanced' );
-  add_meta_box( 'omb_image_info', __( 'Image Info', 'omb-metabox' ), array( $this, 'omb_render_image' ), 'post', 'advanced' );
-  add_meta_box( 'omb_gallery_info', __( 'Gallery Info', 'omb-metabox' ), array( $this, 'omb_render_images' ), 'post', 'advanced' );
- }
-
- public function omb_render_image( $post ) {
-  $image_id  = get_post_meta( $post->ID, 'obm_image_id', true );
-  $image_url = get_post_meta( $post->ID, 'obm_image_url', true );
-
-  $label = __( 'Upload Image', 'omb-metabox' );
-  wp_nonce_field( 'omb_image_action', 'omb_image_nonce_field' );
-
-  $image_info_html = <<<EOD
+    $image_info_html = <<<EOD
 		<div id="myImageMetaBox">
 		<label>{$label}</label>
 		<button class="button" id="upload_image">{$label}</button>
@@ -128,17 +126,17 @@ class OMBMetaBox {
 		<input type="hidden" name="obm_image_url" id="obm_image_url" value={$image_url} />
 		<div id="image_container"></div></div>
 EOD;
-  echo $image_info_html;
- }
+    echo $image_info_html;
+  }
 
- public function omb_render_images( $post ) {
-  $images_id  = get_post_meta( $post->ID, 'obm_images_id', true );
-  $images_url = get_post_meta( $post->ID, 'obm_images_url', true );
+  public function omb_render_images($post) {
+    $images_id  = get_post_meta($post->ID, 'obm_images_id', true);
+    $images_url = get_post_meta($post->ID, 'obm_images_url', true);
 
-  $label = __( 'Gallery Images', 'omb-metabox' );
-  wp_nonce_field( 'omb_gallery_action', 'omb_gallery_nonce_field' );
+    $label = __('Gallery Images', 'omb-metabox');
+    wp_nonce_field('omb_gallery_action', 'omb_gallery_nonce_field');
 
-  $image_info_html = <<<EOD
+    $image_info_html = <<<EOD
 		<div id="myGalleryMetaBox">
 		<label>{$label}</label>
 		<button class="button" id="upload_images">{$label}</button>
@@ -147,33 +145,33 @@ EOD;
 		<input type="hidden" name="obm_images_url" id="obm_images_url" value={$images_url} />
 		<div id="gallery_container"></div></div>
 EOD;
-  echo $image_info_html;
- }
+    echo $image_info_html;
+  }
 
- public function omb_render_metabox( $post ) {
-  $location = get_post_meta( $post->ID, 'obm_location', true );
-  $country  = get_post_meta( $post->ID, 'obm_country', true );
+  public function omb_render_metabox($post) {
+    $location = get_post_meta($post->ID, 'obm_location', true);
+    $country  = get_post_meta($post->ID, 'obm_country', true);
 
-  $is_favorite = get_post_meta( $post->ID, 'obm_is_favorite', true );
-  $is_checked  = $is_favorite == 1 ? 'checked' : 0;
+    $is_favorite = get_post_meta($post->ID, 'obm_is_favorite', true);
+    $is_checked  = $is_favorite == 1 ? 'checked' : 0;
 
-  $saved_colors = get_post_meta( $post->ID, 'omb_clr', true );
-  $saved_color  = get_post_meta( $post->ID, 'omb_color', true );
+    $saved_colors = get_post_meta($post->ID, 'omb_clr', true);
+    $saved_color  = get_post_meta($post->ID, 'omb_color', true);
 
-  $omb_dp          = get_post_meta( $post->ID, 'omb_dp', true );
-  $saved_fav_color = get_post_meta( $post->ID, 'omb_fav_color', true );
+    $omb_dp          = get_post_meta($post->ID, 'omb_dp', true);
+    $saved_fav_color = get_post_meta($post->ID, 'omb_fav_color', true);
 
-  $label1 = __( 'Location', 'omb-metabox' );
-  $label2 = __( 'Country', 'omb-metabox' );
-  $label3 = __( 'Is Favorite', 'omb-metabox' );
+    $label1 = __('Location', 'omb-metabox');
+    $label2 = __('Country', 'omb-metabox');
+    $label3 = __('Is Favorite', 'omb-metabox');
 
-  $label4   = __( 'Colors', 'omb-metabox' );
-  $dp_label = __( 'Date', 'omb-metabox' );
-  $colors   = array( 'red', 'green', 'blue', 'black' );
+    $label4   = __('Colors', 'omb-metabox');
+    $dp_label = __('Date', 'omb-metabox');
+    $colors   = array('red', 'green', 'blue', 'black');
 
-  wp_nonce_field( 'omb_location', 'omb_location_field' );
+    wp_nonce_field('omb_location', 'omb_location_field');
 
-  $metabox_html = <<<EOD
+    $metabox_html = <<<EOD
 		<label for="obm_location">{$label1}</label>
 		<input type="text" name="obm_location" id="obm_location" value="{$location}"  />
 		<br />
@@ -185,39 +183,39 @@ EOD;
 	<br />
 			<label>{$label4}</label>
 EOD;
-  $saved_colors = is_array( $saved_colors ) ? $saved_colors : array();
-  foreach ( $colors as $color ) {
-   $_color  = ucwords( $color );
-   $checked = in_array( $color, $saved_colors ) ? 'checked' : '';
-   $metabox_html .= <<<EOD
+    $saved_colors = is_array($saved_colors) ? $saved_colors : array();
+    foreach ($colors as $color) {
+      $_color  = ucwords($color);
+      $checked = in_array($color, $saved_colors) ? 'checked' : '';
+      $metabox_html .= <<<EOD
 <label for="omb_clr_{$color}">{$_color}</label>
 <input type="checkbox" name="omb_clr[]" id="omb_clr_{$color}" value="{$color}" {$checked} />
 
 EOD;
-  }
-  foreach ( $colors as $color ) {
-   $_color  = ucwords( $color );
-   $checked = ( $color == $saved_color ) ? "checked='checked'" : '';
-   $metabox_html .= <<<EOD
+    }
+    foreach ($colors as $color) {
+      $_color  = ucwords($color);
+      $checked = ($color == $saved_color) ? "checked='checked'" : '';
+      $metabox_html .= <<<EOD
 		<p>
 <label for="omb_color_{$color}">{$_color}</label>
 <input type="radio" name="omb_color" id="omb_color_{$color}" value="{$color}" {$checked} />
 </p>
 EOD;
-  }
+    }
 
-  $metabox_html .= <<<EOD
+    $metabox_html .= <<<EOD
 <label for="omb_dp">{$dp_label}</label>
 <input type="text" name="omb_dp" id="omb_dp" value="{$omb_dp}" />
 EOD;
 
-  $dropdown_html = "<option>" . __( 'Select a color', 'text-domain' ) . "</option>";
-  foreach ( $colors as $color ) {
-   $_color   = ucwords( $color );
-   $selected = ( $color == $saved_fav_color ) ? "selected" : '';
-   $dropdown_html .= sprintf( "<option %s value='%s'>%s</option>", $selected, $color, $_color );
-  }
-  $metabox_html .= <<<EOD
+    $dropdown_html = "<option>" . __('Select a color', 'text-domain') . "</option>";
+    foreach ($colors as $color) {
+      $_color   = ucwords($color);
+      $selected = ($color == $saved_fav_color) ? "selected" : '';
+      $dropdown_html .= sprintf("<option %s value='%s'>%s</option>", $selected, $color, $_color);
+    }
+    $metabox_html .= <<<EOD
 
 <label for="omb_dp">{$label4}</label>
 <select name="omb_fav_color" id="omb_fav_color">
@@ -225,8 +223,8 @@ EOD;
 </select>
 EOD;
 
-  echo $metabox_html;
- }
+    echo $metabox_html;
+  }
 }
 
 new OMBMetaBox();
--- meta-fields-texanomy.php
+++ meta-fields-texanomy.php
@@ -1,48 +1,71 @@
 <?php
- $args = array(
-  'type'              => 'text',
-  'description'       => __( 'Type your extra data here', 'text-domain' ),
-  'sanitize_callback' => 'sanitize_text_field',
-  'single'            => true,
-  'show_in_rest'      => true,
- );
- register_meta( 'post', 'extra_info_meta', $args );
+$woocom_category = 'woocom';
+$args = array(
+	'type'              => 'text',
+	'description'       => __('Type your extra data here', 'text-domain'),
+	'sanitize_callback' => 'sanitize_text_field',
+	'single'            => true,
+	'show_in_rest'      => true,
+);
+register_meta('post', 'extra_info_meta', $args);
 
- function wc_category_add_form_field() {
- ?>
+function wc_category_add_form_field() {
+?>
 	<div class="form-field form-required term-name-wrap">
-		<label for="extra-info"><?php _e( 'Extra Info *', 'text-domain' )?></label>
-		<input name="extra-info" id="extra-info" type="text" value="" size="40" aria-required="true">
-		<p><?php _e( 'The extra info is how it appears on your site.', 'text-domain' )?></p>
+		<label for="extra_info"><?php _e('Extra Info *', 'text-domain') ?></label>
+		<input name="extra_info" id="extra_info" type="text" value="">
+		<p><?php _e('The extra info is how it appears on your site.', 'text-domain') ?></p>
 	</div>
+
+
+	<div class="form-field">
+		<label for="selected_page"><?php esc_attr_e('Page select', 'mytheme'); ?></label>
+		<?php wp_dropdown_pages(array('name' => 'selected_page', 'id' => 'selected_page', 'show_option_none' => '--')); ?>
+		<p><?php esc_attr_e('Select the page you want to show the content of in your category.', 'mytheme'); ?></p>
+	</div>
+
 <?php
- }
+}
 
- add_action( 'category_add_form_fields', 'wc_category_add_form_field' );
+add_action('category_add_form_fields', 'wc_category_add_form_field');
 
- function wc_category_edit_form_field( $term ) {
-  $extra_info = get_term_meta( $term->term_id, 'extra_info_meta', true );
- ?>
+function wc_category_edit_form_field($term) {
+	$selected_page = get_term_meta($term->term_id, 'selected_page', true);
+	$extra_info = get_term_meta($term->term_id, 'extra_info', true);
+?>
 
 	<tr class="form-field form-required term-name-wrap">
-		<th scope="row">
-			<label for="extra-info"><?php _e( 'Extra Info *', 'text-domain' )?></label>
-		</th>
+		<th scope="row"><label for="extra_info"><?php _e('Extra Info *', 'text-domain') ?></label></th>
 		<td>
-			<input name="extra-info" id="extra-info" type="text" value="<?php echo esc_attr( $extra_info ); ?>" size="40" aria-required="true" />
-			<p><?php _e( 'The extra info is how it appears on your site.', 'text-domain' )?></p>
+			<input name="extra_info" id="extra_info" type="text" value="<?php echo $extra_info; ?>">
+			<p><?php _e(' The extra info is how it appears on your site.', 'text-domain') ?></p>
 		</td>
 	</tr>
+
+	<tr class="form-field">
+		<th scope="row"><label for="selected_page"><?php esc_attr_e('Page select', 'mytheme'); ?></label></th>
+		<td>
+			<?php wp_dropdown_pages(array('name' => 'selected_page', 'id' => 'selected_page', 'selected' => $selected_page, 'show_option_none' => '--')); ?>
+			<p class="description"><?php esc_attr_e('Select the page you want to show the content of in your category.', 'mytheme'); ?></p>
+		</td>
+	</tr>
+
 <?php
- }
+}
 
- add_action( 'category_edit_form_fields', 'wc_category_edit_form_field' );
+add_action('category_edit_form_fields', 'wc_category_edit_form_field');
 
- function taxm_save_category_meta( $term ) {
-  if ( wp_verify_nonce( $_POST['_wpnonce_add-tag'], 'add-tag' ) ) {
-   $extra_info = sanitize_text_field( $_POST['extra-info'] );
-   update_term_meta( $term->term_id, 'extra_info_meta', $extra_info );
-  }
- }
+function woocom_save_extra_category_fields($term_id) {
+	if (isset($_POST['selected_page'])) {
+		$selected_page = intval($_POST['selected_page']);
+		update_term_meta($term_id, 'selected_page', $selected_page );
+	}
 
-add_action( 'create_category', 'taxm_save_category_meta' );
+	if (isset($_POST['extra_info'])) {
+		$extra_info = $_POST['extra_info'];
+		update_term_meta($term_id, 'extra_info', $extra_info );
+	}
+}
+
+add_action('created_category', 'woocom_save_extra_category_fields');
+add_action('edited_category', 'woocom_save_extra_category_fields');
