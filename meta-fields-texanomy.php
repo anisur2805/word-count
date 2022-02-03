3697bc6cbbac7eab8e3ae7213d11fa14d812a86e
anisur2805 <anisur2805@gmail.com>
2022-02-03 08:41:20 +0600

    meta fields verify nonce

--- meta-fields-texanomy.php
+++ meta-fields-texanomy.php
@@ -1,5 +1,5 @@
 <?php
-$woocom_category = 'woocom';
+$wc_category = 'woocom';
 $args = array(
 	'type'              => 'text',
 	'description'       => __('Type your extra data here', 'text-domain'),
@@ -28,6 +28,7 @@ function wc_category_add_form_field() {
 }
 
 add_action('category_add_form_fields', 'wc_category_add_form_field');
+add_action('post_tag_add_form_fields', 'wc_category_add_form_field');
 
 function wc_category_edit_form_field($term) {
 	$selected_page = get_term_meta($term->term_id, 'selected_page', true);
@@ -54,18 +55,105 @@ function wc_category_edit_form_field($term) {
 }
 
 add_action('category_edit_form_fields', 'wc_category_edit_form_field');
+add_action('post_tag_edit_form_fields', 'wc_category_edit_form_field');
 
-function woocom_save_extra_category_fields($term_id) {
-	if (isset($_POST['selected_page'])) {
-		$selected_page = intval($_POST['selected_page']);
-		update_term_meta($term_id, 'selected_page', $selected_page );
-	}
+function wc_save_extra_category_fields($term_id) {
+	if( wp_verify_nonce( $_POST['_wpnonce'], "update-tag_{$term_id}" )) {
+		// if (isset($_POST['selected_page'])) {
+			$selected_page = intval($_POST['selected_page']);
+			update_term_meta($term_id, 'selected_page', $selected_page );
+		// }
 
-	if (isset($_POST['extra_info'])) {
-		$extra_info = $_POST['extra_info'];
-		update_term_meta($term_id, 'extra_info', $extra_info );
+		// if (isset($_POST['extra_info'])) {
+			$extra_info = $_POST['extra_info'];
+			update_term_meta($term_id, 'extra_info', $extra_info );
+		// }
 	}
 }
 
-add_action('created_category', 'woocom_save_extra_category_fields');
-add_action('edited_category', 'woocom_save_extra_category_fields');
+add_action('created_category', 'wc_save_extra_category_fields');
+add_action('created_post_tag', 'wc_save_extra_category_fields');
+add_action('edited_post_tag', 'wc_save_extra_category_fields');
+add_action('edited_category', 'wc_save_extra_category_fields');
+
+/**
+ * Adding custom column to taxonomy
+ *
+ * @var     array     $cat_columns     Columns array in custom taxonomy
+ * @return  array     Returns updated category columns
+ * @since   1.0.0
+ */
+if( ! function_exists( 'wc_manage_edit_category_columns' ) ) {
+	function wc_manage_edit_category_columns( $cat_columns ) {
+		$cat_columns['selected_page'] = esc_attr__('Selected Page', 'woocom');
+		return $cat_columns;
+	}
+	
+}
+
+/**
+ * Managing custom column in taxonomy display
+ *
+ * @var     array     $deprecated
+ * @var     array     $column_name     Column name
+ * @var     array     $term_id         Taxonomy term id
+ * @since   1.0.0
+ */
+add_filter( 'manage_edit-category_columns', 'wc_manage_edit_category_columns'); 
+add_filter( 'manage_edit-post_tag_columns', 'wc_manage_edit_category_columns'); 
+
+if( !function_exists( 'wc_manage_category_custom_column' ) ) {
+	function wc_manage_category_custom_column( $deprecated, $column_name, $term_id ) {
+		if( $column_name == 'selected_page' ) {
+			$selected_page = get_term_meta( $term_id, 'selected_page', true );
+			if( isset( $selected_page ) && $selected_page != '' ) {
+				echo get_the_title( $selected_page );
+			} 
+		}
+	}
+}
+
+/**
+ * Make column sortable
+ *
+ * @param  array $sortable Sortable columns array
+ * @return array           Updated sortable columns array
+ * @since   1.0.0
+ */
+add_filter( 'manage_category_custom_column', 'wc_manage_category_custom_column', 10, 3 );
+add_filter( 'manage_post_tag_custom_column', 'wc_manage_category_custom_column', 10, 3 );
+
+if( ! function_exists( 'wc_manage_sortable_columns' ) ) {
+	function wc_manage_sortable_columns( $sortable) {
+		$sortable['selected_page'] = 'selected_page';
+		return $sortable;
+	}
+}
+add_filter( 'manage_edit-category_sortable_columns', 'wc_manage_sortable_columns');
+add_filter( 'manage_edit-post_tag_sortable_columns', 'wc_manage_sortable_columns');
+
+/**
+ * Column sortable query
+ *
+ * @param  array $pieces     Terms query SQL clauses.
+ * @param  array $taxonomies An array of taxonomies.
+ * @param  array $args       An array of terms query arguments.
+ * @return array             Modified query array
+ * @since   1.0.0
+ */
+
+if( !function_exists( 'wc_terms_clauses' ) ) {
+	function wc_terms_clauses( $pieces, $taxonomies, $args ) {
+		global $wpdb;
+		
+		$orderby = isset( $_REQUEST['orderby']) ? trim( wp_unslash( $_REQUEST['orderby']) ) : 'selected_page';
+		if( $orderby == 'selected_page') {
+			$pieces['fields'] .= ", trm.*";
+			$pieces['join'] .= " INNER JOIN {$wpdb->termmeta} AS trm ON tt.term_id = trm.term_id";
+			$pieces['orderby'] = "ORDER BY trm.meta_value";
+		}
+		
+		return $pieces;
+	}
+}
+add_filter( 'terms_clauses', 'wc_terms_clauses', 10, 3 );
